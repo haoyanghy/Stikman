083ea700a77a7457c205c52d6c04304f406bb188
haoyang <aarontan113388@gmail.com>
2022-08-13 12:03:29 +0800

    update

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x0392EeBe289002A73421f0911eB05b3f10AaCe9E";
 // URL to extract Metadata of a Stikman NFT
-const METADATA_URL = "https://stikman-ily8e3l9z-haoyanghy.vercel.app/";
+const METADATA_URL = "https://stikman-qokigsj74-haoyanghy.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
